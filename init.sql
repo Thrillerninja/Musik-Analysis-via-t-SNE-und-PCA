03b42b7f0bed92890af116b515396a6e648e7751
Thrillerninja <r.orbach@web.de>
2025-03-19 22:49:14 +0100

    Doc added

--- init.sql
+++ init.sql
@@ -1,21 +1,21 @@
 -- Erweiterung aktivieren
 CREATE EXTENSION IF NOT EXISTS vector;
 
--- Tabelle für Musikdaten entferen (falls vorhanden)
+-- Tabelle fuer Musikdaten entfernen (falls vorhanden)
 DROP TABLE IF EXISTS music_vectors;
 
--- Tabelle für Musikdaten erstellen
+-- Tabelle fuer Musikdaten erstellen
 CREATE TABLE music_vectors (
     id SERIAL PRIMARY KEY,
     features VECTOR(14)  -- 14-dimensionaler Vektor
 );
 
--- Beispiel für das Einfügen eines Vektors
+-- Beispiel fuer das Einfuegen eines Vektors
 -- INSERT INTO music_vectors (features)
 -- VALUES (
 --     '[0.825, 0.652, -3.183, 0.0802, 0.581, 0.0, 0.0931, 0.931, 95.977]'
 -- );
 
--- IVFFLAT-Index für schnelle Nearest-Neighbor-Suchen erstellen
+-- IVFFLAT-Index fuer schnelle Nearest-Neighbor-Suchen erstellen
 CREATE INDEX music_vector_idx ON music_vectors 
 USING ivfflat (features vector_l2_ops) WITH (lists = 100);
